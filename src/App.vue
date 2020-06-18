1c8a2ef642a1bf1cbefa2f61f2d50086e06a6c43
Phillip Linley <61027312+Solus90@users.noreply.github.com>
2020-06-18 11:04:47 -0400

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <Hero msg="Hello World.  Lorem ipsum" title="Welcome to Your Vue.js App using Buefy!"/>
+    <contact title="Contact Us" />
+  </div>
+</template>
+
+<script>
+import Hero from './components/Hero.vue'
+import Contact from './components/Contact.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Hero,
+    Contact
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ src/components/Hero.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="hero is-info is-fullheight is-bold">
+    <div class="hero-body">
+      <div class="container has-text-centered">
+        <h1 class="title">{{ title }}</h1>
+        <p class="is-size-5">{{ msg }}</p>
+        <a href="#contactForm" class="button m-t-10">Contact Us</a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Hero',
+  props: {
+    title: String,
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+    .m-t-10{
+        margin-top: 10px;
+    }
+</style>
